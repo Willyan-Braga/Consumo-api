d58967afa36c23e7d72ae49d30abb8b8a53053d0
Willyan Braga <willyan.braga00@gmail.com>
2022-12-02 21:23:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import api from './src/services/api';
+import Filmes from './src/Filmes/index';
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state ={
+      filmes: []
+    }
+  }
+
+  async componentDidMount(){
+    const response = await api.get('r-api/?api=filmes');
+    this.setState({
+      filmes: response.data
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <FlatList
+        data={this.state.filmes}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => <Filmes data={item}/>}
+        />
+      </View>
+    )
+  }
+
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +43,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 100,
   },
 });
